f47dee7250ef405350c6fb28144acb1369daabc2
Merlcules <57169555+Merlcules@users.noreply.github.com>
2019-10-30 15:09:01 +0100

    User Dialogue erstellt

--- src/com/company/UserDialogue.java
+++ src/com/company/UserDialogue.java
@@ -1,4 +1,36 @@
 package com.company;
+import java.util.Scanner;
 
 public class UserDialogue {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    int getUserInput(String label, String... alternatives){
+        do{
+            promptUser(label, alternatives);
+            int input = scanUserInput();
+            if(input < 0|| input>= alternatives.length){
+                System.out.println("Fehler! Das ist keine gültige Eingabe!");
+            } else{
+                return input;
+            }
+        }while(true);
+    }
+
+    private int scanUserInput() {
+        try {
+            String inputString = scanner.nextLine();
+            return Integer.parseInt(inputString);
+        } catch (NumberFormatException nfe) {
+            return -1;
+        }
+    }
+    private void promptUser(String label, String... alternatives) {
+        System.out.print(label);
+        int index = 0;
+        for (String alternative : alternatives) {
+            System.out.format("(%d) %s ", index++, alternative);
+        }
+        System.out.println();
+    }
 }
