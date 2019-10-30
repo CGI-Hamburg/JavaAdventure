ce2aad39b68a69532891eedd320e00ee6685e38b
furytooth <57170959+furytooth@users.noreply.github.com>
2019-10-30 15:06:16 +0100

    git commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        thffghjghvdfgdfgfdfdgdfgdfgdff
-                asjdskldjfsdljfdsydf
-                NEUEHDNDH
-                        BHDHDHD
+       git commit
     }
 }
