bbcedbf209aa19e20341922309880a57c500c3dc
Merlcules <57169555+Merlcules@users.noreply.github.com>
2019-11-29 07:51:49 +0100

    Room class

---
+++ src/com/company/Rooms.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Rooms {
+
+    private int numberDoors;
+    private String[] directions;
+    private String roomName;
+
+    public String[] getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String[] directions) {
+        this.directions = directions;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public int getNumberDoors() {
+        return numberDoors;
+    }
+
+    public void setNumberDoors(int numberDoors) {
+        this.numberDoors = numberDoors;
+    }
+
+
+}
